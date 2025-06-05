8cb45dd9028edd98406ad1fc02a73ad79a9507fd
JoaoVictorCRP <joaovictorxl120@gmail.com>
2025-06-04 23:59:50 -0300

    variables types

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "app_name" {
-  type = "string"
+  type = string
 }
 
 variable "aws_region" {
-  type = "string"
+  type    = string
   default = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  type = string
+  type    = string
   default = "10.0.0.0/16"
 }
 

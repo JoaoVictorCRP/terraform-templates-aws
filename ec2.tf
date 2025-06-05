9207e5b7c0bbdfca1d394c0880cdea6922649a08
JoaoVictorCRP <joaovictorxl120@gmail.com>
2025-06-04 23:03:51 -0300

    feat | added http and https SGs

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,13 @@ resource "aws_instance" "main_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [
+    aws_security_group.ssh
+    aws_security_group.http_https
+  ]
   associate_public_ip_address = true
 
   tags = {
-    Name = "main-ec2"
+    Name = var.app_name
   }
 }
--- security_group.tf
+++ security_group.tf
@@ -1,7 +1,7 @@
 # Security Group
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-sg"
-  description = "Allow SSH"
+resource "aws_security_group" "ssh" {
+  name        = "public-ssh"
+  description = "Allows SSH"
   vpc_id      = aws_vpc.main_vpc.id
 
   ingress {
@@ -19,6 +19,37 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   tags = {
-    Name = "ec2-sg"
+    Name = "public-ssh-sg"
+  }
+}
+
+resource "aws_security_group" "http_https" {
+  name        = "http-https"
+  description = "Allows HTTP and HTTPS"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public-http-https-sg"
   }
 }

2ed91e7478e363c30a980f28da71701f3681025e
JoaoVictorCRP <joaovictorxl120@gmail.com>
2025-06-04 23:00:36 -0300

    feat | removing availability zone

--- network.tf
+++ network.tf
@@ -10,7 +10,6 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = var.subnet_cidr
-  availability_zone = var.availability_zone
 
   tags = {
     Name = "public-subnet"
--- variables.tf
+++ variables.tf
@@ -7,10 +7,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "availability_zone" {
-  type = string
-}
-
 variable "vpc_cidr" {
   type = string
   default = "10.0.0.0/16"
